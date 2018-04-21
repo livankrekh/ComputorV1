89b49f2689939212aa8cc4911a1f04a88a3e3263
livankrekh <livannnn@yandex.ru>
2018-04-21 19:37:19 +0300

    50%

--- computor.py
+++ computor.py
@@ -1,12 +1,37 @@
 import sys
 import re
 
+def getPower(str):
+	if str.find('x') != -1 or str.find('X') != -1:
+		if str.find('^') == -1:
+			return 1
+		else:
+			return int(str[str.find('^') + 1])
+	else:
+		return 0
+
 def parser(str_exp):
 	str_exp = str_exp.replace(' ', '')
 	polynom_arr = str_exp.split('=')
 	res = []
-	tmp = {"sign": 0, "coff": 0, "power": 0}
-	print(polynom_arr)
+	exmp = {"koff": 0, "power": 0}
+	for tmp2 in polynom_arr[0].split('+'):
+		if tmp2.find('-') != -1:
+			split_minus = tmp2.split('-')
+			exmp["koff"] = int(split_minus[0])
+			exmp["power"] = getPower(split_minus[0])
+			del split_minus[0]
+			res.append(exmp)
+			for tmp3 in split_minus:
+				exmp["koff"] = int(tmp3) * -1
+				exmp["power"] = getPower(tmp3)
+				res.append(exmp)
+		else:
+			exmp["koff"] = int(tmp2)
+			exmp["power"] = getPower(tmp2)
+			res.append(exmp)
+
+	print(res)
 
 if __name__ == "__main__":
 	if len(sys.argv) > 2:
